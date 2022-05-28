960278cff7239d72a373fd688ed1e36c66135ceb
honey170 <63653475+hingjae@users.noreply.github.com>
2022-05-29 01:17:52 +0900

    set real time

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,17 @@
 const clock = document.querySelector(".clock");
 
-clock.innerText = "15:33";
+// padStart 문자열을 길게 만들어야 할 때 사용
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+// setInterval(sayHello, 1000);
+// setTimeout(sayHello, 1000); only one time
+
+getClock();
+setInterval(getClock, 1000);
