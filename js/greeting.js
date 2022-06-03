e544d2baf04f5fa94ef36f2c2ea26512aab25d4d
honey170 <63653475+hingjae@users.noreply.github.com>
2022-06-03 23:16:20 +0900

    m

--- js/greeting.js
+++ js/greeting.js
@@ -2,6 +2,7 @@ const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const btn = document.querySelector(".btn");
 const greeting = document.querySelector(".greeting");
+const todoForm = document.querySelector(".todo-form");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -18,6 +19,7 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username} !`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  todoForm.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
--- js/todo.js
+++ js/todo.js
@@ -2,7 +2,14 @@ const toDoForm = document.querySelector(".todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".todo-list");
 
-const toDos = [];
+let toDos = [];
+const TODOS_KEY = "todos";
+const savedToDos = localStorage.getItem(TODOS_KEY);
+// JSON.stringify(toDos) --> 문자열 형태로 저장
+// JSON.parse("[1,2,3,4,5]") ==> [1,2,3,4,5]
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
@@ -25,8 +32,19 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj.text);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
